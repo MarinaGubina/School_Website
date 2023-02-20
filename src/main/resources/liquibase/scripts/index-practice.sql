199155a26326e1d4122291cebc9577ddfe5d6d2e
Marina Gubina <gubinamarina1998@gmail.com>
2023-02-20 13:36:00 +0300

    доработка

--- src/main/resources/liquibase/scripts/index-practice.sql
+++ src/main/resources/liquibase/scripts/index-practice.sql
@@ -1,7 +1,11 @@
 -- liquibase formatted sql
 
 -- changeset mgubina:1
+--precondition-sql-check expectedResult:1 select case when count(i.tablename) = 1 then 0 else 1 end from pg_tables t inner join pg_indexes i on t.tablename = i.tablename where t.tablename = 'student' and i.indexname ='student_name_index';
+--onFail=MARK_RAN
 CREATE INDEX student_name_index ON student (name);
 
 -- changeset mgubina:2
-CREATE INDEX faculty_nc_index ON faculty (name, color);
+--precondition-sql-check expectedResult:1 select case when count(i.tablename) = 1 then 0 else 1 end from pg_tables t inner join pg_indexes i on t.tablename = i.tablename where t.tablename = 'faculty' and i.indexname ='faculty_nc_index';
+--onFail=MARK_RAN
+CREATE UNIQUE INDEX faculty_nc_index ON faculty (name, color);
